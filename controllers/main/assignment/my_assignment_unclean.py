4cf76a7eef4e6a8f82078b3d2a0b35442b092c48
Lahouar <sameh.lahouar@epfl.ch>
2025-04-25 14:29:36 +0200

    Increased Robustness

--- controllers/main/assignment/my_assignment_unclean.py
+++ controllers/main/assignment/my_assignment_unclean.py
@@ -1,10 +1,11 @@
 import numpy as np
+import time
 import cv2
 import sys
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-import time
+# import exercises.ex3_motion_planner as mp
+# import exercises.ex1_pid_control as pid
+# import exercises.ex0_rotations as rot
 
 # The available ground truth state measurements can be accessed by calling sensor_data[item]. All values of "item" are provided as defined in main.py within the function read_sensors. 
 # The "item" values that you may later retrieve for the hardware project are:
@@ -36,21 +37,32 @@ camera_params = {
 
 S_WIDTH = 300
 S_HEIGHT = 300
+frames = []
+poses = []
+nb_frames = 2
 control_command = [0, 0, 0, 0]  # Initialize control command
 f_pixel = 161.01
+right_scan = False
+left_scan = False
+center_aligned = False
+go_forward = False
 takeoff = False
 slope_threshold = 0.3
 inside_door = False 
 door_pos = [[0,0,0]]
 turn_counter = 0
 intial_pos = [0, 0, 0, 0]
+grid_size = 0.25
+bounds = (0, 5, 0, 3, 0, 1.5)
+step = 0
 offset_correct = [[0,0,0]]
+fast_laps = False
 path = [[0,0,0]]
 stupid_path = False
 
 def get_command(sensor_data, camera_data, dt):
 
-    global control_command, inside_door, door_pos, turn_counter, initial_pos, offset_correct, fast_laps, stupid_path, path
+    global frames, poses, control_command, right_scan, left_scan, center_aligned, go_forward, inside_door, door_pos, turn_counter, initial_pos, grid_size, bounds, step, offset_correct, fast_laps, stupid_path, path
     # NOTE: Displaying the camera image with cv2.imshow() will throw an error because GUI operations should be performed in the main thread.
     # If you want to display the camera image you can call it main.py.
     global takeoff
@@ -83,33 +95,70 @@ def get_command(sensor_data, camera_data, dt):
         if len(door_pos) == 6:
             if offset_correct[0] == [0,0,0] or distance_points(initial_pos, offset_correct[0]) < 0.5:
                 offset_correct.pop(0)
+            corr_pos = [intial_pos[0], initial_pos[1], initial_pos[2], initial_pos[3]]
             waypoints = [door_pos[0], offset_correct[0], offset_correct[1], door_pos[1], offset_correct[2], door_pos[2], offset_correct[3], door_pos[3], offset_correct[4], door_pos[4], door_pos[5]]
+            fast_laps = True
             waypoints_4D = [wp if len(wp) == 4 else list(wp) + [0.0] for wp in waypoints]
             command = path_planning(sensor_data, dt, waypoints_4D, tol=0.1)
             return command
+            print("Waypoints: ", waypoints)
+            print("Door positions: ", door_pos)
+            print("Offset positions: ", offset_correct)
+            # door_pos_tuples = [tuple(pos) for pos in door_pos]
+            # print("Door positions tuple: ", door_pos_tuples)
+            # planner = mp.MotionPlanner3D(door_pos_tuples, [[0,0,0,0,0,0]], bounds, grid_size, (0, 0, 0))
+            # trajectory = planner.trajectory_setpoints
+            # times = planner.time_setpoints
+
+            # if step == 0:
+            #     if distance(sensor_data, initial_pos) < 0.3:
+            #         step = step + 1
+            #     return initial_pos
+            
+            # setpoint = [waypoints[step-1][0], waypoints[step-1][1], waypoints[step-1][2], sensor_data['yaw']]
+            # # pwm = controller.setpoint_to_pwm(dt, setpoint, sensor_data)
+            # if distance(sensor_data, waypoints[step-1]) < 0.2:
+            #     step = step + 1
+            #     if step == len(waypoints)+1:
+            #         step = 0
+            # print("Step: ", step)
+            # # pwm = rot.rot_body2inertial(pwm, euler_angles, quaternion)
+            # return setpoint
     
     if stupid_path:
         waypoints_4D = [wp if len(wp) == 4 else list(wp) + [0.0] for wp in path]
         for wp in waypoints_4D:
                 if wp[3] < 0:
                     wp[3] = 0.0
+        print("Waypoints: ", waypoints_4D)
         command = path_planning(sensor_data, dt, waypoints_4D, tol=0.2)
         return command
 
 
     # Define the drawing frame
     drawing_frame = camera_data.copy()
-
+    
+    # Draw the detected parallelogram on the drawing frame
+    # drawing_frame = draw_parallelogram(drawing_frame, detect_parallelogram(camera_data))
     parallelogram = detect_parallelogram(camera_data)
 
     if turn_counter > 300 :
         # Reset the flags if no parallelogram is detected after 20 turns
+        right_scan = False
+        left_scan = False
+        center_aligned = False
+        go_forward = False
+        print("Door positions: ", door_pos)
         if len(door_pos) < 5 :
             stupid_path = True
         else: return initial_pos
 
     if not(purple_color_detected(drawing_frame, pixel_threshold= 200)):
         # No Purle detected, reset the flags
+        right_scan = False
+        left_scan = False
+        center_aligned = False
+        go_forward = False
         turn_counter += 1
         return rotate(sensor_data, 15)  # Rotate to search for the parallelogram  
     elif purple_color_detected(drawing_frame, pixel_threshold= 40000) and not inside_door:
@@ -129,11 +178,20 @@ def get_command(sensor_data, camera_data, dt):
 
     if parallelogram is None and inside_door:
         # No parallelogram detected, reset the flags
+        right_scan = False
+        left_scan = False
+        center_aligned = False
+        go_forward = False
         inside_door = False
         return rotate(sensor_data, 15)
 
     if parallelogram is None and purple_color_detected(drawing_frame, pixel_threshold= 200):
         # No parallelogram detected, reset the flags
+        right_scan = False
+        left_scan = False
+        center_aligned = False
+        go_forward = False
+        #return rotate(sensor_data, 15)  # Rotate to search for the parallelogram
         return move_right(sensor_data, distance= 2)  # Move right to search for the parallelogram
     
     inside_door = False
@@ -146,11 +204,56 @@ def get_command(sensor_data, camera_data, dt):
         centroid = np.mean(points, axis=0)
         cv2.circle(drawing_frame, (int(centroid[0]), int(centroid[1])), 5, (255, 0, 0), -1) 
 
+        # centroid = choose_target(sensor_data, camera_data)
+        # cv2.circle(drawing_frame, (int(centroid[0]), int(centroid[1])), 5, (255, 0, 0), -1) 
+        # print("centroid = ", centroid)
+
         if is_center_aligned(centroid, camera_data):
             return move_towards_camera_point(sensor_data, centroid, camera_data, 0.7)
 
         if centroid is not None:
-            return align_center(centroid, sensor_data, camera_data)           
+            #center_aligned = True
+            return align_center(centroid, sensor_data, camera_data)
+        
+
+        
+        
+        # slope = paral_slope(parallelogram)  
+        # if center_aligned and slope < -0.5:
+        #     center_aligned = False
+        #     go_forward = True
+        #     slope_coeff = 2 * abs(slope)
+        #     #return move_right(sensor_data, slope_coeff)
+        #     return move_right_PID(sensor_data, distance= slope_coeff)
+        # elif center_aligned and slope > 0.5:
+        #     center_aligned = False
+        #     go_forward = True
+        #     slope_coeff = 2 * abs(slope)
+        #     # return rotate_on_circle(sensor_data, centroid, 3, 0.1, dt)
+        #     return move_left_PID(sensor_data, distance= slope_coeff)
+        # if centroid is not None and not center_aligned:
+        #     #print("distance = ", abs(centroid[0] - sensor_data['z_global']))
+        #     center_aligned = True
+        #     # return align_center(centroid, sensor_data, camera_data)
+        # else :
+        #     # center_aligned = True
+        #     go_forward = True
+        #     return move_towards_camera_point(sensor_data, centroid, camera_data, 0.5)
+
+        # if centroid is not None and not center_aligned:
+        #     #print("distance = ", abs(centroid[0] - sensor_data['z_global']))
+        #     center_aligned = True
+        #     return align_center(centroid, sensor_data, camera_data)
+        
+
+        # if go_forward and center_aligned :
+        #     return move_towards_camera_point(sensor_data, centroid, camera_data, 0.5)
+        
+
+        # print("Slope: ", slope)
+        
+        
+            
 
     current_pose = ([sensor_data["x_global"], sensor_data["y_global"], sensor_data["z_global"]], 
                     [sensor_data["q_x"], sensor_data["q_y"], sensor_data["q_z"], sensor_data["q_w"]])
@@ -171,6 +274,7 @@ def align_center(center, sensor_data, camera_data):
     control_command = [sensor_data['x_global'], sensor_data['y_global'], z, yaw]
     return control_command
 
+import numpy as np
 
 def is_center_aligned(center, camera_data, threshold_px=100):
     err_x = center[0] - camera_data.shape[1] / 2
@@ -189,6 +293,8 @@ def move_right(sensor_data, distance = 0.5):
     tar_y = y - distance * np.cos(yaw)
 
     control_command = [tar_x, tar_y, sensor_data['z_global'], sensor_data['yaw']]
+
+    #control_command = rot.rot_body2inertial(control_command, [sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']], [sensor_data['q_x'], sensor_data['q_y'], sensor_data['q_z'], sensor_data['q_w']])
 
     return control_command
 
@@ -329,6 +435,154 @@ def paral_slope(parallelogram_points):
     slope = (y2 - y1) / (x2 - x1)
     return slope
 
+def PID_command(center, angle, sensor_data):
+    """
+    Generates a PID control command based on the detected center of the parallelogram and the drone's current state.
+
+    Args:
+        center: Tuple containing the x and y coordinates of the detected center.
+        angle: Angle of the door.
+        sensor_data: Dictionary containing the current sensor data.
+    """
+
+    err_x = center[0] - S_WIDTH / 2
+    err_y = -(center[1] - S_HEIGHT / 2)
+    err_angle = angle
+    print(err_angle)
+    # Proportional gains
+    kp_y = 0.005
+    kp_z = 0.3
+    kp_yaw = 0.005
+    orientation = np.array([sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']])
+    R = rot.euler2rotmat(orientation)
+    loc = R.T @ np.array([sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']])
+    x_local, y_local, z_local = loc[0], loc[1], loc[2]
+    x_pid = x_local + 0.5
+    y_pid = y_local - kp_y * err_angle
+    z_pid = z_local + kp_z * err_y
+    glob = R @ np.array([x_pid, y_pid, z_pid])
+    command_x, command_y, command_z = glob[0], glob[1], glob[2]
+    command_yaw = sensor_data['yaw'] - kp_yaw * err_x
+
+    return [command_x, command_y, command_z, command_yaw]
+
+
+
+def move_right_PID(sensor_data, distance=0.5, kp=1.0):
+    """
+    Move right by applying a control in the drone's local frame, 
+    then transform it to the global frame using orientation.
+    """
+    # Orientation and position
+    orientation = np.array([sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']])
+    R = rot.euler2rotmat(orientation)  # Rotation matrix from drone body to world
+
+    # Target movement in local frame: +Y (right in body frame)
+    target_local = np.array([0.0, kp * distance, 0.0])  # no change in z, just sidestep right
+    current_pos = np.array([sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']])
+
+    # Transform to global frame
+    target_global_offset = R @ target_local
+    target_pos = current_pos + target_global_offset
+    ang = np.deg2rad(15)
+
+    # Return target position with current yaw (no rotation change)
+    return [target_pos[0], target_pos[1], target_pos[2], sensor_data['yaw']+ang]
+
+def move_left_PID(sensor_data, distance=0.5, kp=1.0):
+    """
+    Move left by applying a control in the drone's local frame, 
+    then transform it to the global frame using orientation.
+    """
+    orientation = np.array([sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']])
+    R = rot.euler2rotmat(orientation)
+
+    # Target movement in local frame: -Y (left in body frame)
+    target_local = np.array([0.0, -kp * distance, 0.0])
+    current_pos = np.array([sensor_data['x_global'], sensor_data['y_global'], sensor_data['z_global']])
+
+    target_global_offset = R @ target_local
+    target_pos = current_pos + target_global_offset
+    ang = np.deg2rad(-15)
+
+    return [target_pos[0], target_pos[1], target_pos[2], sensor_data['yaw']+ang]
+
+def rotate_on_circle(sensor_data, center, radius, angular_speed, dt):
+    """
+    Makes the drone rotate on a circular path of a given radius.
+
+    Args:
+        sensor_data: Dictionary containing the current sensor data.
+        center: Tuple (x, y) representing the center of the circle.
+        radius: Radius of the circular path (in meters).
+        angular_speed: Angular speed of rotation (in radians per second).
+        dt: Time step for the control loop (in seconds).
+
+    Returns:
+        control_command: List containing the control command [x, y, z, yaw].
+    """
+    # Current position of the drone
+    x_current = sensor_data["x_global"]
+    y_current = sensor_data["y_global"]
+    z_current = sensor_data["z_global"]
+    yaw_current = sensor_data["yaw"]
+
+    # Calculate the angle of the drone relative to the center
+    angle = np.arctan2(y_current - center[1], x_current - center[0])
+
+    # Update the angle to move along the circle
+    angle += angular_speed * dt  # Positive angular speed for counterclockwise, negative for clockwise
+
+    # Calculate the new position on the circular path
+    x_new = center[0] + radius * np.cos(angle)
+    y_new = center[1] + radius * np.sin(angle)
+    z_new = z_current  # Maintain the same altitude
+
+    # Calculate the yaw to face the center of the circle
+    yaw_new = np.arctan2(center[1] - y_new, center[0] - x_new)
+
+    # Generate the control command
+    control_command = [x_new, y_new, z_new, yaw_new]
+
+    return control_command
+
+def distance_to_target(sensor_data, target):
+    """
+    Calculate the distance from the drone to a target point.
+
+    Args:
+        sensor_data: Dictionary containing the current sensor data.
+        target: Tuple (x, y) representing the target point.
+
+    Returns:
+        distance: Distance from the drone to the target point.
+    """
+    x_drone = sensor_data['x_global']
+    y_drone = sensor_data['y_global']
+
+    distance = np.sqrt((x_drone - target[0]) ** 2 + (y_drone - target[1]) ** 2)
+    return distance
+
+def choose_target(sensor_data, camera_data):
+    """
+    Choose the closest target from a list of targets.
+
+    Args:
+        sensor_data: Dictionary containing the current sensor data.
+        targets: List of target points [(x1, y1), (x2, y2), ...].
+
+    Returns:
+        closest_target: The closest target point to the drone.
+    """
+
+    centers = get_parallelogram_centers(camera_data)
+
+    distances = [distance_to_target(sensor_data, center) for center in centers]
+    closest_target_index = np.argmin(distances)
+    closest_target = centers[closest_target_index]
+    
+    return closest_target
+
 def distance(sensor_data, previous_sensor_data):
     """
     Calculate the distance between the current and previous sensor data.
@@ -352,7 +606,7 @@ def distance(sensor_data, previous_sensor_data):
     
     return distance
 
-def distance_points(point1, point2):
+def distance_points(point1, point2 ):
     """
     Calculate the distance between two points in 3D space.
 
@@ -465,6 +719,11 @@ def get_parallelogram_centers(frame):
         centers.append(tuple(centroid))
 
     return centers
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 on_ground = True
 height_desired = 0.5
